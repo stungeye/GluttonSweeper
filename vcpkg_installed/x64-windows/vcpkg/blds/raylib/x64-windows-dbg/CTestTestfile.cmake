9e138f83651885293e0ac5d070d580711355aa64
Kyle Geske <stungeye@gmail.com>
2025-10-22 12:04:24 -0500

    Remove vcpkg_installed folder from repository and add to .gitignore

--- vcpkg_installed/x64-windows/vcpkg/blds/raylib/x64-windows-dbg/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/kgeske/source/repos/GluttonSweeper/vcpkg_installed/x64-windows/vcpkg/blds/raylib/src/5.5-966575b391.clean
-# Build directory: C:/Users/kgeske/source/repos/GluttonSweeper/vcpkg_installed/x64-windows/vcpkg/blds/raylib/x64-windows-dbg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
