d1a6161470247a74898763e0d820bdd772e005da
Kyle Geske <stungeye@gmail.com>
2025-10-23 21:56:05 -0500

    Comments for the GameContext.

--- GluttonSweeper/GameContext.hpp
+++ GluttonSweeper/GameContext.hpp
@@ -4,8 +4,20 @@ class GameManager;
 class TextureManager;
 
 // Context holds references to all game-wide systems.
-// Currently only GameManager, but could hold more in the future.
-// This allows easy extension (TextureManager, AudioManager, etc.)
+// Currently only GameManager and TextureManager, but could hold 
+// more in the future. (AudioManger, ParticleManager, etc.)
+//
+// Screens can access the context via their GetContext().
+// This avoids the need to pass multiple references around.
+//
+// GameContext is a simple struct with public members.
+//
+// Example of a Screen accessing the gameManager via context:
+//   auto& context = GetContext();
+//   context.gameManager.AddScore(10);
+// 
+// Or more concisely:
+//   GetContext().gameManager.AddScore(10);
 struct GameContext {
     GameManager& gameManager;
 	TextureManager& textureManager;
